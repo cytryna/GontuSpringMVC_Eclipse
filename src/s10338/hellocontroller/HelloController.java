a81ea595b6761f77f2b7e2b9dd8bb56c63341fe6
rwichrowski <radoslaw.wichrowski@efigence.com>
2016-02-16 07:38:54 +0100

    Gontu 7 lesson

--- src/s10338/hellocontroller/HelloController.java
+++ src/s10338/hellocontroller/HelloController.java
@@ -1,18 +1,36 @@
 package s10338.hellocontroller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
-public class HelloController extends AbstractController {
 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
-		ModelAndView modelAndView = new ModelAndView("HelloPage");
-		modelAndView.addObject("welcomeMessage", "Hi User, welcome to the first Spring Application");
-		return modelAndView;
+@Controller
+public class HelloController {
+
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+		ModelAndView model = new ModelAndView("HelloPage");
+		model.addObject("welcomeMessage", "Hi Radek, welcome to the first Spring Application");
+		return model;
 	}
-
 }
+
+
+//package s10338.hellocontroller;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.servlet.mvc.AbstractController;
+//
+//public class HelloController extends AbstractController {
+//
+//	@Override
+//	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+//		ModelAndView modelAndView = new ModelAndView("HelloPage");
+//		modelAndView.addObject("welcomeMessage", "Hi Radek, welcome to the first Spring Application");
+//		return modelAndView;
+//	}
+//}
